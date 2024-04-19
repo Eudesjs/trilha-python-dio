d696ccc0dd3c3236b39d35b8a4e211de1f681cf1
Eudes Silva | Transformando Linhas de Código em Soluções Inovadoras <134567785+Eudesjs@users.noreply.github.com>
2024-04-19 12:40:48 -0300

    Update tipos_de_dados.py

--- 00 - Fundamentos/tipos_de_dados.py
+++ 00 - Fundamentos/tipos_de_dados.py
@@ -1,5 +1,5 @@
-print(11 + 10 + 1000)
-print(1.5 + 1 + 0.5)
-print(True)
-print(False)
-print("Python")
+print(11 + 10 + 1000) # TIPO INTEIRO
+print(1.5 + 1 + 0.5) # TIPO FLOAT
+print(True) # TIPO BOOLEANO OU BOOL
+print(False) # TIPO BOOLEANO OU BOOL
+print("Python") # TIPO STRING
